d57a6b8a2a563835d21020db242410fe968c00bc
Henry7233 <nobellin30@gmail.com>
2026-09-07 17:17:23 +0800

    Myo,I modified upload.html.

---
+++ app/routes/client.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, flash, redirect, render_template, session, url_for
+
+
+client_bp = Blueprint("client", __name__, url_prefix="/client")
+
+
+@client_bp.route("/upload", methods=["GET", "POST"])
+def upload():
+    if "user_id" not in session:
+        flash("Please log in first.", "error")
+        return redirect(url_for("auth.login"))
+
+    return render_template("client/upload.html", username=session.get("user_email"))
